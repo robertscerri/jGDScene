13b5ce9f75af7fa2627c0de64391704e3be9b331
Robert Scerri <robert.scerri2@gmail.com>
2023-12-09 18:57:10 +0100

    added rootNode property to GDScene class

--- src/main/java/com/robertscerri/GDScene.java
+++ src/main/java/com/robertscerri/GDScene.java
@@ -4,14 +4,22 @@ public class GDScene {
     protected int loadSteps;
     protected int format;
 
+    protected Node rootNode;
+
     public GDScene() {
         this.loadSteps = 4;
         this.format = 3;
+        this.rootNode = new Node("RootNode", "Node2D");
     }
 
     public GDScene(int loadSteps, int format) {
         this.loadSteps = loadSteps;
         this.format = format;
+    }
+
+    public GDScene(int loadSteps, int format, Node rootNode) {
+        this(loadSteps, format);
+        this.rootNode = rootNode;
     }
 
     public int getLoadSteps() {
