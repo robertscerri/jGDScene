eb6aa3908723da662b5d53a9571083dd1aa74a10
Robert Scerri <robert.scerri2@gmail.com>
2023-12-10 17:38:04 +0100

    created Resource class

---
+++ src/main/java/com/robertscerri/ExtResource.java
@@ -0,0 +1,23 @@
+package com.robertscerri;
+
+public class ExtResource extends Resource {
+    public ExtResource() {
+        super("ext_resource");
+    }
+
+    public ExtResource(String type, String path) {
+        super("ext_resource", type);
+
+        this.setHeadingAttribute("path", path);
+    }
+
+    public ExtResource(String type, String path, String id) {
+        super("ext_resource", type, id);
+
+        this.setHeadingAttribute("path", path);
+    }
+
+    public String getReference() {
+        return "ExtResource(\n" + this.getId() + "\")";
+    }
+}
--- src/main/java/com/robertscerri/SubResource.java
+++ src/main/java/com/robertscerri/SubResource.java
@@ -1,56 +1,16 @@
 package com.robertscerri;
 
-import java.util.Random;
-
-public class SubResource extends SceneEntryWithProps implements Referable {
+public class SubResource extends Resource {
     public SubResource() {
         super("sub_resource");
-
-        this.setHeadingAttribute("type", "");
-        this.setHeadingAttribute("id", "");
     }
 
     public SubResource(String type) {
-        super("sub_resource");
-
-        this.setHeadingAttribute("type", type);
-        this.setHeadingAttribute("id", generateID(type));
+        super("sub_resource", type);
     }
 
     public SubResource(String type, String id) {
-        super("sub_resource");
-
-        this.setHeadingAttribute("type", type);
-        this.setHeadingAttribute("id", id);
-    }
-
-    public String getType() {
-        return (String) this.getHeadingAttribute("type");
-    }
-
-    public void setType(String type) {
-        this.setHeadingAttribute("type", type);
-    }
-
-    public String getId() {
-        return (String) this.getHeadingAttribute("id");
-    }
-
-    public void setId(String id) {
-        this.setHeadingAttribute("id", id);
-    }
-
-    //ID generated is not guaranteed to be unique
-    private String generateID(String type) {
-        StringBuilder suffix = new StringBuilder();
-        final String CHAR_SET = "abcdefghijklmnopqrstuvwxyz0123456789";
-
-        for (int i = 0; i < 5; i++) {
-            char c = CHAR_SET.charAt(new Random().nextInt(CHAR_SET.length()));
-            suffix.append(c);
-        }
-
-        return type + "_" + suffix;
+        super("sub_resource", type, id);
     }
 
     public String getReference() {
