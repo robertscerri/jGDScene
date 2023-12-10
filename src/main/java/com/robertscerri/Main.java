5a128439d382b3efc54d0848fa15eb70b9002b44
Robert Scerri <robert.scerri2@gmail.com>
2023-12-10 16:26:37 +0100

    testing SubResource objects as properties

--- src/main/java/com/robertscerri/Main.java
+++ src/main/java/com/robertscerri/Main.java
@@ -10,16 +10,22 @@ public class Main {
         bird.setProperty("position", "Vector2(106, 192)");
         rootNode.addChild(bird);
 
+        SubResource circleShape2D1 = new SubResource("CircleShape2D");
+        circleShape2D1.setProperty("radius", 7.0);
+
+        SubResource circleShape2D2 = new SubResource("CircleShape2D");
+        circleShape2D2.setProperty("radius", 8.0);
+
         Node collisionShape2D = new Node("CollisionShape2D", "CollisionShape2D");
-        collisionShape2D.setProperty("shape", "SubResource(\"CircleShape2D_mta1t\")");
-        collisionShape2D.setProperty("metadata/_edit_lock_", "true");
+        collisionShape2D.setProperty("shape", circleShape2D1);
+        collisionShape2D.setProperty("metadata/_edit_lock_", true);
         bird.addChild(collisionShape2D);
 
         Node area2D = new Node("Area2D", "Area2D");
         bird.addChild(area2D);
 
         Node collisionShape2D2 = new Node("CollisionShape2D", "CollisionShape2D");
-        collisionShape2D2.setProperty("shape", "SubResource(\"CircleShape2D_ixabh\")");
+        collisionShape2D2.setProperty("shape", circleShape2D2);
         area2D.addChild(collisionShape2D2);
 
         gdScene.writeToFile("C:\\Users\\Robert\\Desktop\\scene.tscn");
