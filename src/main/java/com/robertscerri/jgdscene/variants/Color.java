494ab6c7994fec70d953ca67f704fb4703df1849
Robert Scerri <robert.scerri2@gmail.com>
2024-07-30 19:09:43 +0200

    Colour variant now supports values greater than 255

--- src/main/java/com/robertscerri/jgdscene/variants/Color.java
+++ src/main/java/com/robertscerri/jgdscene/variants/Color.java
@@ -2,41 +2,32 @@ package com.robertscerri.jgdscene.variants;
 
 import java.awt.color.ColorSpace;
 
-public class Color extends java.awt.Color {
+public class Color {
+    public int r;
+    public int g;
+    public int b;
+    public int a;
+
     public Color(int r, int g, int b) {
-        super(r, g, b);
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.a = 255;
     }
 
     public Color(int r, int g, int b, int a) {
-        super(r, g, b, a);
-    }
-
-    public Color(int rgb) {
-        super(rgb);
-    }
-
-    public Color(int rgba, boolean hasalpha) {
-        super(rgba, hasalpha);
-    }
-
-    public Color(float r, float g, float b) {
-        super(r, g, b);
-    }
-
-    public Color(float r, float g, float b, float a) {
-        super(r, g, b, a);
-    }
-
-    public Color(ColorSpace cspace, float[] components, float alpha) {
-        super(cspace, components, alpha);
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.a = a;
     }
 
     @Override
     public String toString() {
-        double r = this.getRed() / 255.0;
-        double g = this.getGreen() / 255.0;
-        double b = this.getBlue() / 255.0;
-        double a = this.getAlpha() / 255.0;
+        double r = this.r / 255.0;
+        double g = this.g / 255.0;
+        double b = this.b / 255.0;
+        double a = this.a / 255.0;
 
         return "Color(%f, %f, %f, %f)".formatted(r, g, b, a);
     }
