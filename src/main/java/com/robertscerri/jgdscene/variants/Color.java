a544f9c3a3f1d38dc3e27e41a913ee00c3cbd75a
Robert Scerri <robert.scerri2@gmail.com>
2024-11-17 17:48:23 +0100

    added several Color variant implementations

--- src/main/java/com/robertscerri/jgdscene/variants/Color.java
+++ src/main/java/com/robertscerri/jgdscene/variants/Color.java
@@ -1,16 +1,193 @@
 package com.robertscerri.jgdscene.variants;
 
 public class Color {
+    public static final Color ALICE_BLUE = new Color(0.941176f, 0.972549f, 1f, 1f);
+    public static final Color ANTIQUE_WHITE = new Color(0.980392f, 0.921569f, 0.843137f, 1f);
+    public static final Color AQUA = new Color(0f, 1f, 1f, 1f);
+    public static final Color AQUAMARINE = new Color(0.498039f, 1f, 0.831373f, 1f);
+    public static final Color AZURE = new Color(0.941176f, 1f, 1f, 1f);
+    public static final Color BEIGE = new Color(0.960784f, 0.960784f, 0.862745f, 1f);
+    public static final Color BISQUE = new Color(1f, 0.894118f, 0.768627f, 1f);
+    public static final Color BLACK = new Color(0, 0,0, 1);
+    public static final Color BLANCHED_ALMOND = new Color(1f, 0.921569f, 0.803922f, 1f);
+    public static final Color BLUE = new Color(0f, 0f, 1f, 1f);
+    public static final Color BLUE_VIOLET = new Color(0.541176f, 0.168627f, 0.886275f, 1f);
+    public static final Color BROWN = new Color(0.647059f, 0.164706f, 0.164706f, 1f);
+    public static final Color BURLYWOOD = new Color(0.870588f, 0.721569f, 0.529412f, 1f);
+    public static final Color CADET_BLUE = new Color(0.372549f, 0.619608f, 0.627451f, 1f);
+    public static final Color CHARTREUSE = new Color(0.498039f, 1f, 0f, 1f);
+    public static final Color CHOCOLATE = new Color(0.823529f, 0.411765f, 0.117647f, 1f);
+    public static final Color CORAL = new Color(1f, 0.498039f, 0.313726f, 1f);
+    public static final Color CORNFLOWER_BLUE = new Color(0.392157f, 0.584314f, 0.929412f, 1f);
+    public static final Color CORNSILK = new Color(1f, 0.972549f, 0.862745f, 1f);
+    public static final Color CRIMSON = new Color(0.862745f, 0.0784314f, 0.235294f, 1f);
+    public static final Color CYAN = new Color(0f, 1f, 1f, 1f);
+    public static final Color DARK_BLUE = new Color(0f, 0f, 0.545098f, 1f);
+    public static final Color DARK_CYAN = new Color(0f, 0.545098f, 0.545098f, 1f);
+    public static final Color DARK_GOLDENROD = new Color(0.721569f, 0.52549f, 0.0431373f, 1f);
+    public static final Color DARK_GRAY = new Color(0.662745f, 0.662745f, 0.662745f, 1f);
+    public static final Color DARK_GREEN = new Color(0f, 0.392157f, 0f, 1f);
+    public static final Color DARK_KHAKI = new Color(0.741176f, 0.717647f, 0.419608f, 1f);
+    public static final Color DARK_MAGENTA = new Color(0.545098f, 0f, 0.545098f, 1f);
+    public static final Color DARK_OLIVE_GREEN = new Color(0.333333f, 0.419608f, 0.184314f, 1f);
+    public static final Color DARK_ORANGE = new Color(1f, 0.54902f, 0f, 1f);
+    public static final Color DARK_ORCHID = new Color(0.6f, 0.196078f, 0.8f, 1f);
+    public static final Color DARK_RED = new Color(0.545098f, 0f, 0f, 1f);
+    public static final Color DARK_SALMON = new Color(0.913725f, 0.588235f, 0.478431f, 1f);
+    public static final Color DARK_SEA_GREEN = new Color(0.560784f, 0.737255f, 0.560784f, 1f);
+    public static final Color DARK_SLATE_BLUE = new Color(0.282353f, 0.239216f, 0.545098f, 1f);
+    public static final Color DARK_SLATE_GRAY = new Color(0.184314f, 0.309804f, 0.309804f, 1f);
+    public static final Color DARK_TURQUOISE = new Color(0f, 0.807843f, 0.819608f, 1f);
+    public static final Color DARK_VIOLET = new Color(0.580392f, 0f, 0.827451f, 1f);
+    public static final Color DEEP_PINK = new Color(1f, 0.0784314f, 0.576471f, 1f);
+    public static final Color DEEP_SKY_BLUE = new Color(0f, 0.74902f, 1f, 1f);
+    public static final Color DIM_GRAY = new Color(0.411765f, 0.411765f, 0.411765f, 1f);
+    public static final Color DODGER_BLUE = new Color(0.117647f, 0.564706f, 1f, 1f);
+    public static final Color FIREBRICK = new Color(0.698039f, 0.133333f, 0.133333f, 1f);
+    public static final Color FLORAL_WHITE = new Color(1f, 0.980392f, 0.941176f, 1f);
+    public static final Color FOREST_GREEN = new Color(0.133333f, 0.545098f, 0.133333f, 1f);
+    public static final Color FUCHSIA = new Color(1f, 0f, 1f, 1f);
+    public static final Color GAINSBORO = new Color(0.862745f, 0.862745f, 0.862745f, 1f);
+    public static final Color GHOST_WHITE = new Color(0.972549f, 0.972549f, 1f, 1f);
+    public static final Color GOLD = new Color(1f, 0.843137f, 0f, 1f);
+    public static final Color GOLDENROD = new Color(0.854902f, 0.647059f, 0.12549f, 1f);
+    public static final Color GRAY = new Color(0.745098f, 0.745098f, 0.745098f, 1f);
+    public static final Color GREEN = new Color(0f, 1f, 0f, 1f);
+    public static final Color GREEN_YELLOW = new Color(0.678431f, 1f, 0.184314f, 1f);
+    public static final Color HONEYDEW = new Color(0.941176f, 1f, 0.941176f, 1f);
+    public static final Color HOT_PINK = new Color(1f, 0.411765f, 0.705882f, 1f);
+    public static final Color INDIAN_RED = new Color(0.803922f, 0.360784f, 0.360784f, 1f);
+    public static final Color INDIGO = new Color(0.294118f, 0f, 0.509804f, 1f);
+    public static final Color IVORY = new Color(1f, 1f, 0.941176f, 1f);
+    public static final Color KHAKI = new Color(0.941176f, 0.901961f, 0.54902f, 1f);
+    public static final Color LAVENDER = new Color(0.901961f, 0.901961f, 0.980392f, 1f);
+    public static final Color LAVENDER_BLUSH = new Color(1f, 0.941176f, 0.960784f, 1f);
+    public static final Color LAWN_GREEN = new Color(0.486275f, 0.988235f, 0f, 1f);
+    public static final Color LEMON_CHIFFON = new Color(1f, 0.980392f, 0.803922f, 1f);
+    public static final Color LIGHT_BLUE = new Color(0.678431f, 0.847059f, 0.901961f, 1f);
+    public static final Color LIGHT_CORAL = new Color(0.941176f, 0.501961f, 0.501961f, 1f);
+    public static final Color LIGHT_CYAN = new Color(0.878431f, 1f, 1f, 1f);
+    public static final Color LIGHT_GOLDENROD = new Color(0.980392f, 0.980392f, 0.823529f, 1f);
+    public static final Color LIGHT_GRAY = new Color(0.827451f, 0.827451f, 0.827451f, 1f);
+    public static final Color LIGHT_GREEN = new Color(0.564706f, 0.933333f, 0.564706f, 1f);
+    public static final Color LIGHT_PINK = new Color(1f, 0.713726f, 0.756863f, 1f);
+    public static final Color LIGHT_SALMON = new Color(1f, 0.627451f, 0.478431f, 1f);
+    public static final Color LIGHT_SEA_GREEN = new Color(0.12549f, 0.698039f, 0.666667f, 1f);
+    public static final Color LIGHT_SKY_BLUE = new Color(0.529412f, 0.807843f, 0.980392f, 1f);
+    public static final Color LIGHT_SLATE_GRAY = new Color(0.466667f, 0.533333f, 0.6f, 1f);
+    public static final Color LIGHT_STEEL_BLUE = new Color(0.690196f, 0.768627f, 0.870588f, 1f);
+    public static final Color LIGHT_YELLOW = new Color(1f, 1f, 0.878431f, 1f);
+    public static final Color LIME = new Color(0f, 1f, 0f, 1f);
+    public static final Color LIME_GREEN = new Color(0.196078f, 0.803922f, 0.196078f, 1f);
+    public static final Color LINEN = new Color(0.980392f, 0.941176f, 0.901961f, 1f);
+    public static final Color MAGENTA = new Color(1f, 0f, 1f, 1f);
+    public static final Color MAROON = new Color(0.690196f, 0.188235f, 0.376471f, 1f);
+    public static final Color MEDIUM_AQUAMARINE = new Color(0.4f, 0.803922f, 0.666667f, 1f);
+    public static final Color MEDIUM_BLUE = new Color(0f, 0f, 0.803922f, 1f);
+    public static final Color MEDIUM_ORCHID = new Color(0.729412f, 0.333333f, 0.827451f, 1f);
+    public static final Color MEDIUM_PURPLE = new Color(0.576471f, 0.439216f, 0.858824f, 1f);
+    public static final Color MEDIUM_SEA_GREEN = new Color(0.235294f, 0.701961f, 0.443137f, 1f);
+    public static final Color MEDIUM_SLATE_BLUE = new Color(0.482353f, 0.407843f, 0.933333f, 1f);
+    public static final Color MEDIUM_SPRING_GREEN = new Color(0f, 0.980392f, 0.603922f, 1f);
+    public static final Color MEDIUM_TURQUOISE = new Color(0.282353f, 0.819608f, 0.8f, 1f);
+    public static final Color MEDIUM_VIOLET_RED = new Color(0.780392f, 0.0823529f, 0.521569f, 1f);
+    public static final Color MIDNIGHT_BLUE = new Color(0.0980392f, 0.0980392f, 0.439216f, 1f);
+    public static final Color MINT_CREAM = new Color(0.960784f, 1f, 0.980392f, 1f);
+    public static final Color MISTY_ROSE = new Color(1f, 0.894118f, 0.882353f, 1f);
+    public static final Color MOCCASIN = new Color(1f, 0.894118f, 0.709804f, 1f);
+    public static final Color NAVAJO_WHITE = new Color(1f, 0.870588f, 0.678431f, 1f);
+    public static final Color NAVY_BLUE = new Color(0f, 0f, 0.501961f, 1f);
+    public static final Color OLD_LACE = new Color(0.992157f, 0.960784f, 0.901961f, 1f);
+    public static final Color OLIVE = new Color(0.501961f, 0.501961f, 0f, 1f);
+    public static final Color OLIVE_DRAB = new Color(0.419608f, 0.556863f, 0.137255f, 1f);
+    public static final Color ORANGE = new Color(1f, 0.647059f, 0f, 1f);
+    public static final Color ORANGE_RED = new Color(1f, 0.270588f, 0f, 1f);
+    public static final Color ORCHID = new Color(0.854902f, 0.439216f, 0.839216f, 1f);
+    public static final Color PALE_GOLDENROD = new Color(0.933333f, 0.909804f, 0.666667f, 1f);
+    public static final Color PALE_GREEN = new Color(0.596078f, 0.984314f, 0.596078f, 1f);
+    public static final Color PALE_TURQUOISE = new Color(0.686275f, 0.933333f, 0.933333f, 1f);
+    public static final Color PALE_VIOLET_RED = new Color(0.858824f, 0.439216f, 0.576471f, 1f);
+    public static final Color PAPAYA_WHIP = new Color(1f, 0.937255f, 0.835294f, 1f);
+    public static final Color PEACH_PUFF = new Color(1f, 0.854902f, 0.72549f, 1f);
+    public static final Color PERU = new Color(0.803922f, 0.521569f, 0.247059f, 1f);
+    public static final Color PINK = new Color(1f, 0.752941f, 0.796078f, 1f);
+    public static final Color PLUM = new Color(0.866667f, 0.627451f, 0.866667f, 1f);
+    public static final Color POWDER_BLUE = new Color(0.690196f, 0.878431f, 0.901961f, 1f);
+    public static final Color PURPLE = new Color(0.627451f, 0.12549f, 0.941176f, 1f);
+    public static final Color REBECCA_PURPLE = new Color(0.4f, 0.2f, 0.6f, 1f);
+    public static final Color RED = new Color(1f, 0f, 0f, 1f);
+    public static final Color ROSY_BROWN = new Color(0.737255f, 0.560784f, 0.560784f, 1f);
+    public static final Color ROYAL_BLUE = new Color(0.254902f, 0.411765f, 0.882353f, 1f);
+    public static final Color SADDLE_BROWN = new Color(0.545098f, 0.270588f, 0.0745098f, 1f);
+    public static final Color SALMON = new Color(0.980392f, 0.501961f, 0.447059f, 1f);
+    public static final Color SANDY_BROWN = new Color(0.956863f, 0.643137f, 0.376471f, 1f);
+    public static final Color SEA_GREEN = new Color(0.180392f, 0.545098f, 0.341176f, 1f);
+    public static final Color SEASHELL = new Color(1f, 0.960784f, 0.933333f, 1f);
+    public static final Color SIENNA = new Color(0.627451f, 0.321569f, 0.176471f, 1f);
+    public static final Color SILVER = new Color(0.752941f, 0.752941f, 0.752941f, 1f);
+    public static final Color SKY_BLUE = new Color(0.529412f, 0.807843f, 0.921569f, 1f);
+    public static final Color SLATE_BLUE = new Color(0.415686f, 0.352941f, 0.803922f, 1f);
+    public static final Color SLATE_GRAY = new Color(0.439216f, 0.501961f, 0.564706f, 1f);
+    public static final Color SNOW = new Color(1f, 0.980392f, 0.980392f, 1f);
+    public static final Color SPRING_GREEN = new Color(0f, 1f, 0.498039f, 1f);
+    public static final Color STEEL_BLUE = new Color(0.27451f, 0.509804f, 0.705882f, 1f);
+    public static final Color TAN = new Color(0.823529f, 0.705882f, 0.54902f, 1f);
+    public static final Color TEAL = new Color(0f, 0.501961f, 0.501961f, 1f);
+    public static final Color THISTLE = new Color(0.847059f, 0.74902f, 0.847059f, 1f);
+    public static final Color TOMATO = new Color(1f, 0.388235f, 0.278431f, 1f);
+    public static final Color TRANSPARENT = new Color(1, 1, 1, 0);
+    public static final Color TURQUOISE = new Color(0.25098f, 0.878431f, 0.815686f, 1f);
+    public static final Color VIOLET = new Color(0.933333f, 0.509804f, 0.933333f, 1f);
+    public static final Color WEB_GRAY = new Color(0.501961f, 0.501961f, 0.501961f, 1f);
+    public static final Color WEB_GREEN = new Color(0f, 0.501961f, 0f, 1f);
+    public static final Color WEB_MAROON = new Color(0.501961f, 0f, 0f, 1f);
+    public static final Color WEB_PURPLE = new Color(0.501961f, 0f, 0.501961f, 1f);
+    public static final Color WHEAT = new Color(0.960784f, 0.870588f, 0.701961f, 1f);
+    public static final Color WHITE = new Color(1f, 1f, 1f, 1f);
+    public static final Color WHITE_SMOKE = new Color(0.960784f, 0.960784f, 0.960784f, 1f);
+    public static final Color YELLOW = new Color(1f, 1f, 0f, 1f);
+    public static final Color YELLOW_GREEN = new Color(0.603922f, 0.803922f, 0.196078f, 1f);
+
     public float r;
     public float g;
     public float b;
     public float a;
 
+    public Color() {
+        this.r = 0;
+        this.g = 0;
+        this.b = 0;
+        this.a = 0;
+    }
+
+    public Color(Color from, float alpha) {
+        this.r = from.r;
+        this.g = from.g;
+        this.b = from.b;
+        this.a = alpha;
+    }
+
+    public Color(Color from) {
+        this.r = from.r;
+        this.g = from.g;
+        this.b = from.b;
+        this.a = from.a;
+    }
+
+    //TODO: Add constructor from hex
+
     public Color(float r, float g, float b) {
         this.r = r;
         this.g = g;
         this.b = b;
-        this.a = 255;
+        this.a = 1.0f;
+    }
+
+    public Color(long r, long g, long b) {
+        this.r = r / 255.0f;
+        this.g = g / 255.0f;
+        this.b = b / 255.0f;
+        this.a = 1.0f;
     }
 
     public Color(float r, float g, float b, float a) {
@@ -18,6 +195,117 @@ public class Color {
         this.g = g;
         this.b = b;
         this.a = a;
+    }
+
+    public Color(long r, long g, long b, long a) {
+        this.r = r / 255.0f;
+        this.g = g / 255.0f;
+        this.b = b / 255.0f;
+        this.a = a / 255.0f;
+    }
+
+    //TODO: Add blend()
+
+    public Color clamp(Color min, Color max) {
+        return new Color(Math.max(min.r, Math.min(this.r, max.r)), Math.max(min.g, Math.min(this.g, max.g)), Math.max(min.b, Math.min(this.b, max.b)), Math.max(min.a, Math.min(this.a, max.a)));
+    }
+
+    //TODO: Add darkened(), from_hsl, from_ok_hsl, from_rgbe9995, from_string, get_luminance
+
+    public static Color hex(int hex) {
+        long a = hex & 0xff;
+        long b = (hex & 0xff00) >> 8;
+        long g = (hex & 0xff0000) >> 16;
+        long r = (hex & 0xff000000) >> 24;
+
+        return new Color(r, g, b, a);
+    }
+
+    public static Color hex64(long hex) {
+        long a = hex & 0xffff;
+        long b = (hex & 0xffff0000L) >> 16;
+        long g = (hex & 0xffff00000000L) >> 32;
+        long r = (hex & 0xffff000000000000L) >> 48;
+
+        return new Color(r, g, b, a);
+    }
+
+    //TODO: Add html(), html_is_valid()
+
+    public Color inverted() {
+        return new Color(1 - this.r, 1 - this.g, 1 - this.b, a);
+    }
+
+    //TODO: Add is_equal_approx(), lerp(), lightened(), linear_to_srgb(), srgb_to_linear()
+
+    public int to_abgr32() {
+        int r = ((int) this.r * 255);
+        int g = ((int) this.g * 255) << 8;
+        int b = ((int) this.b * 255) << 16;
+        int a = ((int) this.a * 255) << 24;
+
+        return r + g + b + a;
+    }
+
+    public long to_abgr64() {
+        long r = ((long) this.r * 255);
+        long g = ((long) this.g * 255) << 16;
+        long b = ((long) this.b * 255) << 32;
+        long a = ((long) this.a * 255) << 48;
+
+        return r + g + b + a;
+    }
+
+    public int to_argb32() {
+        int b = ((int) this.b * 255);
+        int g = ((int) this.g * 255) << 8;
+        int r = ((int) this.r * 255) << 16;
+        int a = ((int) this.a * 255) << 24;
+
+        return r + g + b + a;
+    }
+
+    public long to_argb64() {
+        long b = ((long) this.b * 255);
+        long g = ((long) this.g * 255) << 16;
+        long r = ((long) this.r * 255) << 32;
+        long a = ((long) this.a * 255) << 48;
+
+        return r + g + b + a;
+    }
+
+    //TODO: Add to_html()
+
+    public int to_rgba32() {
+        int a = ((int) this.a * 255);
+        int b = ((int) this.b * 255) << 8;
+        int g = ((int) this.g * 255) << 16;
+        int r = ((int) this.r * 255) << 24;
+
+        return r + g + b + a;
+    }
+
+    public long to_rgba64() {
+        long a = ((long) this.a * 255);
+        long b = ((long) this.b * 255) << 16;
+        long g = ((long) this.g * 255) << 32;
+        long r = ((long) this.r * 255) << 48;
+
+        return r + g + b + a;
+    }
+
+    //TODO: Add != overload
+
+    public Color multiply(Color right) {
+        return new Color(this.r * right.r, this.g * right.g, this.b * right.b, this.a * right.a);
+    }
+
+    public Color multiply(float right) {
+        return new Color(this.r * right, this.g * right, this.b * right, this.a * right);
+    }
+
+    public Color multiply(int right) {
+        return new Color(this.r * right, this.g * right, this.b * right, this.a * right);
     }
 
     public Color add(Color other) {
@@ -28,21 +316,22 @@ public class Color {
         return new Color(this.r - other.r, this.g - other.g, this.b - other.b, this.a - other.a);
     }
 
-    public Color multiply(Color other) {
-        return new Color(this.r * other.r, this.g * other.g, this.b * other.b, this.a * other.a);
+    public Color divide(Color right) {
+        return new Color(this.r / right.r, this.g / right.g, this.b / right.b, this.a / right.a);
     }
 
-    public Color divide(Color other) {
-        return new Color(this.r / other.r, this.g / other.g, this.b / other.b, this.a / other.a);
+    public Color divide(float right) {
+        return new Color(this.r / right, this.g / right, this.b / right, this.a / right);
     }
+
+    public Color divide(int right) {
+        return new Color(this.r / right, this.g / right, this.b / right, this.a / right);
+    }
+
+    //TODO: Add == overload
 
     @Override
     public String toString() {
-        float r = this.r / 255.0f;
-        float g = this.g / 255.0f;
-        float b = this.b / 255.0f;
-        float a = this.a / 255.0f;
-
-        return "Color(%f, %f, %f, %f)".formatted(r, g, b, a);
+        return "Color(%f, %f, %f, %f)".formatted(this.r, this.g, this.b, this.a);
     }
 }
