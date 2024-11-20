ed43a1203cda8d736a31f651deb4e251ad3b187a
Robert Scerri <robert.scerri2@gmail.com>
2024-11-20 22:56:37 +0100

    implemented Vector4 and Vector4i classes

--- src/main/java/com/robertscerri/jgdscene/variants/vectors/Vector4.java
+++ src/main/java/com/robertscerri/jgdscene/variants/vectors/Vector4.java
@@ -1,8 +1,9 @@
 package com.robertscerri.jgdscene.variants.vectors;
-//TODO: UPDATE
+
+import com.robertscerri.jgdscene.utils.FloatUtils;
 import com.robertscerri.jgdscene.variants.Variant;
 
-public class Vector4 extends Variant {
+public class Vector4 extends Variant implements Comparable<Vector4> {
     public static final int AXIS_X = 0;
     public static final int AXIS_Y = 1;
     public static final int AXIS_Z = 2;
@@ -48,28 +49,227 @@ public class Vector4 extends Variant {
         return new Vector4(Math.abs(this.x), Math.abs(this.y), Math.abs(this.z), Math.abs(this.w));
     }
 
-    //TODO: Add the rest of the Vector4 methods
-
-    public double length() {
-        return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2) + Math.pow(this.z, 2) + Math.pow(this.w, 2));
+    public Vector4 ceil() {
+        return new Vector4((float) Math.ceil(this.x), (float) Math.ceil(this.y), (float) Math.ceil(this.z), (float) Math.ceil(this.w));
     }
 
-    public Vector4 add(Vector4 other) {
-        return new Vector4(this.x + other.x, this.y + other.y, this.z + other.z, this.w + other.w);
+    public Vector4 clamp(Vector4 min, Vector4 max) {
+        return new Vector4(FloatUtils.clamp(this.x, min.x, max.x), FloatUtils.clamp(this.y, min.y, max.y), FloatUtils.clamp(this.z, min.z, max.z), FloatUtils.clamp(this.w, min.w, max.w));
     }
 
-    public Vector4 subtract(Vector4 other) {
-        return new Vector4(this.x - other.x, this.y - other.y, this.z - other.z, this.w - other.w);
+    public Vector4 clamp(float min, float max) {
+        return new Vector4(FloatUtils.clamp(this.x, min, max), FloatUtils.clamp(this.y, min, max), FloatUtils.clamp(this.z, min, max), FloatUtils.clamp(this.w, min, max));
     }
 
-    public Vector4 multiply(Vector4 other) {
-        return new Vector4(this.x * other.x, this.y * other.y, this.z * other.z, this.w * other.w);
+    //TODO: cubic_interpolate, cubic_interpolate_in_time
+
+    public Vector4 directionTo(Vector4 to) {
+        return to.subtract(this).normalized();
     }
 
-    public Vector4 divide(Vector4 other) {
-        return new Vector4(this.x / other.x, this.y / other.y, this.z / other.z, this.w / other.w);
+    public float distanceSquaredTo(Vector4 to) {
+        return to.subtract(this).lengthSquared();
     }
 
+    public float distanceTo(Vector4 to) {
+        return to.subtract(this).length();
+    }
+
+    public float dot(Vector4 with) {
+        return (this.x * with.x) + (this.y * with.y) + (this.z * with.z) + (this.w * with.w);
+    }
+
+    public Vector4 floor() {
+        return new Vector4((float) Math.floor(this.x), (float) Math.floor(this.y), (float) Math.floor(this.z), (float) Math.floor(this.w));
+    }
+
+    public Vector4 inverse() {
+        return new Vector4(1 / this.x, 1 / this.y, 1 / this.z, 1 / this.w);
+    }
+
+    public boolean isEqualApprox(Vector4 to) {
+        return FloatUtils.isEqualApprox(this.x, to.x) && FloatUtils.isEqualApprox(this.y, to.y) && FloatUtils.isEqualApprox(this.z, to.z) && FloatUtils.isEqualApprox(this.w, to.w);
+    }
+
+    public boolean isFinite() {
+        return Float.isFinite(this.x) && Float.isFinite(this.y) && Float.isFinite(this.z) && Float.isFinite(this.w);
+    }
+
+    public boolean isNormalized() {
+        return FloatUtils.isEqualApprox(this.length(), 1);
+    }
+
+    public boolean isZeroApprox() {
+        return FloatUtils.isEqualApprox(this.x, 0) && FloatUtils.isEqualApprox(this.y, 0) && FloatUtils.isEqualApprox(this.z, 0) && FloatUtils.isEqualApprox(this.w, 0);
+    }
+
+    public float length() {
+        return (float) Math.sqrt(this.lengthSquared());
+    }
+
+    public float lengthSquared() {
+        return (this.x * this.x) + (this.y * this.y) + (this.z * this.z) + (this.w * this.w);
+    }
+
+    public Vector4 lerp(Vector4 to, float weight) {
+        return new Vector4(FloatUtils.lerp(this.x, to.x, weight), FloatUtils.lerp(this.y, to.y, weight), FloatUtils.lerp(this.z, to.z, weight), FloatUtils.lerp(this.w, to.w, weight));
+    }
+
+    public Vector4 max(Vector4 with) {
+        return new Vector4(Math.max(this.x, with.x), Math.max(this.y, with.y), Math.max(this.z, with.z), Math.max(this.w, with.w));
+    }
+
+    public Vector4 max(float with) {
+        return new Vector4(Math.max(this.x, with), Math.max(this.y, with), Math.max(this.z, with), Math.max(this.w, with));
+    }
+
+    public int maxAxisIndex() {
+        int maxIndex = Vector4.AXIS_X;
+        float maxValue = x;
+
+        if (this.y > maxValue) {
+            maxValue = this.y;
+            maxIndex = Vector4.AXIS_Y;
+        }
+
+        if (this.z > maxValue) {
+            maxValue = this.z;
+            maxIndex = Vector4.AXIS_Z;
+        }
+
+        if (this.w > maxValue) {
+            maxValue = this.w;
+            maxIndex = Vector4.AXIS_W;
+        }
+
+        return maxIndex;
+    }
+
+    public Vector4 min(Vector4 with) {
+        return new Vector4(Math.min(this.x, with.x), Math.min(this.y, with.y), Math.min(this.z, with.z), Math.min(this.w, with.w));
+    }
+
+    public Vector4 min(float with) {
+        return new Vector4(Math.min(this.x, with), Math.min(this.y, with), Math.min(this.z, with), Math.min(this.w, with));
+    }
+
+    public int minAxisIndex() {
+        int maxIndex = Vector4.AXIS_X;
+        float maxValue = x;
+
+        if (this.y <= maxValue) {
+            maxValue = this.y;
+            maxIndex = Vector4.AXIS_Y;
+        }
+
+        if (this.z <= maxValue) {
+            maxValue = this.z;
+            maxIndex = Vector4.AXIS_Z;
+        }
+
+        if (this.w <= maxValue) {
+            maxValue = this.w;
+            maxIndex = Vector4.AXIS_W;
+        }
+
+        return maxIndex;
+    }
+
+    public Vector4 normalized() {
+        return this.divide(this.length());
+    }
+
+    public Vector4 posmod(float mod) {
+        return new Vector4(FloatUtils.posmod(this.x, mod), FloatUtils.posmod(this.y, mod), FloatUtils.posmod(this.z, mod), FloatUtils.posmod(this.w, mod));
+    }
+
+    public Vector4 posmod(Vector4 mod) {
+        return new Vector4(FloatUtils.posmod(this.x, mod.x), FloatUtils.posmod(this.y, mod.y), FloatUtils.posmod(this.z, mod.z), FloatUtils.posmod(this.w, mod.w));
+    }
+
+    public Vector4 round() {
+        return new Vector4(Math.round(this.x), Math.round(this.y), Math.round(this.z), Math.round(this.w));
+    }
+
+    public Vector4 sign() {
+        return new Vector4(Math.signum(this.x), Math.signum(this.y), Math.signum(this.z), Math.signum(this.w));
+    }
+
+    public Vector4 snapped(Vector4 step) {
+        return new Vector4(Math.round(this.x / step.x) * step.x, Math.round(this.y / step.y) * step.y, Math.round(this.z / step.z) * step.z, Math.round(this.w / step.w) * step.w);
+    }
+
+    public Vector4 snapped(float step) {
+        return new Vector4(Math.round(this.x / step) * step, Math.round(this.y / step) * step, Math.round(this.z / step) * step, Math.round(this.w / step) * step);
+    }
+
+    //TODO: Add operator for Projection
+
+    public Vector4 multiply(Vector4 right) {
+        return new Vector4(this.x * right.x, this.y * right.y, this.z * right.z, this.w * right.w);
+    }
+
+    public Vector4 multiply(float right) {
+        return new Vector4(this.x * right, this.y * right, this.z * right, this.w * right);
+    }
+
+    public Vector4 multiply(int right) {
+        return new Vector4(this.x * right, this.y * right, this.z * right, this.w * right);
+    }
+
+    public Vector4 add(Vector4 right) {
+        return new Vector4(this.x + right.x, this.y + right.y, this.z + right.z, this.w + right.w);
+    }
+
+    public Vector4 subtract(Vector4 right) {
+        return new Vector4(this.x - right.x, this.y - right.y, this.z - right.z, this.w - right.w);
+    }
+
+    public Vector4 divide(Vector4 right) {
+        return new Vector4(this.x / right.x, this.y / right.y, this.z / right.z, this.w / right.w);
+    }
+
+    public Vector4 divide(float right) {
+        return new Vector4(this.x / right, this.y / right, this.z / right, this.w / right);
+    }
+
+    public Vector4 divide(int right) {
+        return new Vector4(this.x / right, this.y / right, this.z / right, this.w / right);
+    }
+
+    @Override
+    public int compareTo(Vector4 o) {
+        int comparisonX = Float.compare(this.x, o.x);
+
+        if (comparisonX != 0) {
+            return comparisonX;
+        }
+
+        int comparisonY = Float.compare(this.y, o.y);
+
+        if (comparisonY != 0) {
+            return comparisonY;
+        }
+
+        int comparisonZ = Float.compare(this.z, o.z);
+
+        if (comparisonZ != 0) {
+            return comparisonZ;
+        } else {
+            return Float.compare(this.w, o.w);
+        }
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Vector4 vec) {
+            return this.x == vec.x && this.y == vec.y && this.z == vec.z && this.w == vec.w;
+        }
+
+        return false;
+    }
+
+    @Override
     public String toString() {
         return super.toString("%f, %f, %f, %f".formatted(this.w, this.x, this.y, this.z));
     }
--- src/main/java/com/robertscerri/jgdscene/variants/vectors/Vector4i.java
+++ src/main/java/com/robertscerri/jgdscene/variants/vectors/Vector4i.java
@@ -1,8 +1,9 @@
 package com.robertscerri.jgdscene.variants.vectors;
-//TODO: UPDATE
+
+import com.robertscerri.jgdscene.utils.IntegerUtils;
 import com.robertscerri.jgdscene.variants.Variant;
 
-public class Vector4i extends Variant {
+public class Vector4i extends Variant implements Comparable<Vector4i> {
     public static final int AXIS_X = 0;
     public static final int AXIS_Y = 1;
     public static final int AXIS_Z = 2;
@@ -49,28 +50,175 @@ public class Vector4i extends Variant {
         return new Vector4i(Math.abs(this.x), Math.abs(this.y), Math.abs(this.z), Math.abs(this.w));
     }
 
-    //TODO: Add the rest of the Vector4i methods
-
-    public double length() {
-        return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2) + Math.pow(this.z, 2) + Math.pow(this.w, 2));
+    public Vector4i clamp(Vector4i min, Vector4i max) {
+        return new Vector4i(IntegerUtils.clamp(this.x, min.x, max.x), IntegerUtils.clamp(this.y, min.y, max.y), IntegerUtils.clamp(this.z, min.z, max.z), IntegerUtils.clamp(this.w, min.w, max.w));
     }
 
-    public Vector4i add(Vector4i other) {
-        return new Vector4i(this.x + other.x, this.y + other.y, this.z + other.z, this.w + other.w);
+    public Vector4i clamp(int min, int max) {
+        return new Vector4i(IntegerUtils.clamp(this.x, min, max), IntegerUtils.clamp(this.y, min, max), IntegerUtils.clamp(this.z, min, max), IntegerUtils.clamp(this.w, min, max));
     }
 
-    public Vector4i subtract(Vector4i other) {
-        return new Vector4i(this.x - other.x, this.y - other.y, this.z - other.z, this.w - other.w);
+    public float distanceSquaredTo(Vector4i to) {
+        return to.subtract(this).lengthSquared();
     }
 
-    public Vector4i multiply(Vector4i other) {
-        return new Vector4i(this.x * other.x, this.y * other.y, this.z * other.z, this.w * other.w);
+    public float distanceTo(Vector4i to) {
+        return to.subtract(this).length();
     }
 
-    public Vector4i divide(Vector4i other) {
-        return new Vector4i(this.x / other.x, this.y / other.y, this.z / other.z, this.w / other.w);
+    public float length() {
+        return (float) Math.sqrt(this.lengthSquared());
     }
 
+    public float lengthSquared() {
+        return (this.x * this.x) + (this.y * this.y) + (this.z * this.z) + (this.w * this.w);
+    }
+
+    public Vector4i max(Vector4i with) {
+        return new Vector4i(Math.max(this.x, with.x), Math.max(this.y, with.y), Math.max(this.z, with.z), Math.max(this.w, with.w));
+    }
+
+    public Vector4i max(int with) {
+        return new Vector4i(Math.max(this.x, with), Math.max(this.y, with), Math.max(this.z, with), Math.max(this.w, with));
+    }
+
+    public int maxAxisIndex() {
+        int maxIndex = Vector4i.AXIS_X;
+        float maxValue = x;
+
+        if (this.y > maxValue) {
+            maxValue = this.y;
+            maxIndex = Vector4i.AXIS_Y;
+        }
+
+        if (this.z > maxValue) {
+            maxValue = this.z;
+            maxIndex = Vector4i.AXIS_Z;
+        }
+
+        if (this.w > maxValue) {
+            maxValue = this.w;
+            maxIndex = Vector4i.AXIS_W;
+        }
+
+        return maxIndex;
+    }
+
+    public Vector4i min(Vector4i with) {
+        return new Vector4i(Math.min(this.x, with.x), Math.min(this.y, with.y), Math.min(this.z, with.z), Math.min(this.w, with.w));
+    }
+
+    public Vector4i min(int with) {
+        return new Vector4i(Math.min(this.x, with), Math.min(this.y, with), Math.min(this.z, with), Math.min(this.w, with));
+    }
+
+    public int minAxisIndex() {
+        int maxIndex = Vector4i.AXIS_X;
+        float maxValue = x;
+
+        if (this.y <= maxValue) {
+            maxValue = this.y;
+            maxIndex = Vector4i.AXIS_Y;
+        }
+
+        if (this.z <= maxValue) {
+            maxValue = this.z;
+            maxIndex = Vector4i.AXIS_Z;
+        }
+
+        if (this.w <= maxValue) {
+            maxValue = this.w;
+            maxIndex = Vector4i.AXIS_W;
+        }
+
+        return maxIndex;
+    }
+
+    public Vector4i sign() {
+        return new Vector4i(Integer.signum(this.x), Integer.signum(this.y), Integer.signum(this.z), Integer.signum(this.w));
+    }
+
+    public Vector4i snapped(Vector4i step) {
+        return new Vector4i(Math.round(this.x / (float) step.x) * step.x, Math.round(this.y / (float) step.y) * step.y, Math.round(this.z / (float) step.z) * step.z, Math.round(this.w / (float) step.w) * step.w);
+    }
+
+    public Vector4i snapped(int step) {
+        return new Vector4i(Math.round(this.x / (float) step) * step, Math.round(this.y / (float) step) * step, Math.round(this.z / (float) step) * step, Math.round(this.w / (float) step) * step);
+    }
+
+    public Vector4i modulo(Vector4i right) {
+        return new Vector4i(this.x % right.x, this.y % right.y, this.z % right.z, this.w % right.w);
+    }
+
+    public Vector4i modulo(int right) {
+        return new Vector4i(this.x % right, this.y % right, this.z % right, this.w % right);
+    }
+
+    public Vector4i multiply(Vector4i right) {
+        return new Vector4i(this.x * right.x, this.y * right.y, this.z * right.z, this.w * right.w);
+    }
+
+    public Vector4 multiply(float right) {
+        return new Vector4(this.x * right, this.y * right, this.z * right, this.w * right);
+    }
+
+    public Vector4i multiply(int right) {
+        return new Vector4i(this.x * right, this.y * right, this.z * right, this.w * right);
+    }
+
+    public Vector4i add(Vector4i right) {
+        return new Vector4i(this.x + right.x, this.y + right.y, this.z + right.z, this.w + right.w);
+    }
+
+    public Vector4i subtract(Vector4i right) {
+        return new Vector4i(this.x - right.x, this.y - right.y, this.z - right.z, this.w - right.w);
+    }
+
+    public Vector4i divide(Vector4i right) {
+        return new Vector4i(this.x / right.x, this.y / right.y, this.z / right.z, this.w / right.w);
+    }
+
+    public Vector4 divide(float right) {
+        return new Vector4(this.x / right, this.y / right, this.z / right, this.w / right);
+    }
+
+    public Vector4i divide(int right) {
+        return new Vector4i(this.x / right, this.y / right, this.z / right, this.w / right);
+    }
+
+    @Override
+    public int compareTo(Vector4i o) {
+        int comparisonX = Integer.compare(this.x, o.x);
+
+        if (comparisonX != 0) {
+            return comparisonX;
+        }
+
+        int comparisonY = Integer.compare(this.y, o.y);
+
+        if (comparisonY != 0) {
+            return comparisonY;
+        }
+
+        int comparisonZ = Integer.compare(this.z, o.z);
+
+        if (comparisonZ != 0) {
+            return comparisonZ;
+        } else {
+            return Integer.compare(this.w, o.w);
+        }
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Vector4 vec) {
+            return this.x == vec.x && this.y == vec.y && this.z == vec.z && this.w == vec.w;
+        }
+
+        return false;
+    }
+
+    @Override
     public String toString() {
         return super.toString("%d, %d, %d, %d".formatted(this.w, this.x, this.y, this.z));
     }
