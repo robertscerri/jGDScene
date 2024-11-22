c1596ba80d5aef6d1810fcbd87a57449acc5ec2b
Robert Scerri <robert.scerri2@gmail.com>
2024-11-23 00:38:30 +0100

    added updated Connection

---
+++ src/main/java/com/robertscerri/jgdscene/Connection.java
@@ -0,0 +1,39 @@
+package com.robertscerri.jgdscene;
+
+import com.robertscerri.jgdscene.nodes.Node;
+
+public class Connection {
+    public Node from;
+    public Node to;
+
+    public String signal;
+    public String method;
+
+    public Connection() {
+        this.from = null;
+        this.to = null;
+        this.signal = "";
+        this.method = "";
+    }
+
+    public Connection(Node from, Node to, String signal, String method) {
+        this.from = from;
+        this.to = to;
+        this.signal = signal;
+        this.method = method;
+    }
+
+    public String getHeader() {
+        return "[connection" +
+                " from=" + this.from.getNodePath() + "/" + this.from.getName() +
+                " to=" + this.to.getNodePath() + "/" + this.to.getName() +
+                "]\n";
+    }
+
+    @Override
+    public String toString() {
+        return this.getHeader() +
+                "signal = " + signal + "\n" +
+                "method = " + method + "\n";
+    }
+}
