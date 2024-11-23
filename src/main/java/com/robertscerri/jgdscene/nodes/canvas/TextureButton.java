ed5e0a200d49d1ba0046210ffba89a2deafd651f
Robert Scerri <robert.scerri2@gmail.com>
2024-11-23 01:03:41 +0100

    added Bitmap

--- src/main/java/com/robertscerri/jgdscene/nodes/canvas/TextureButton.java
+++ src/main/java/com/robertscerri/jgdscene/nodes/canvas/TextureButton.java
@@ -1,12 +1,20 @@
 package com.robertscerri.jgdscene.nodes.canvas;
 
 import com.robertscerri.jgdscene.nodes.enums.StretchMode;
+import com.robertscerri.jgdscene.resources.BitMap;
+import com.robertscerri.jgdscene.resources.Texture2D;
 
 public class TextureButton extends BaseButton {
     public Boolean flipH = null;
     public Boolean flipV = null;
     public Boolean ignoreTextureSize = null;
     public StretchMode stretchMode = null;
+    public BitMap textureClickMask = null;
+    public Texture2D textureDisabled = null;
+    public Texture2D textureFocused = null;
+    public Texture2D textureHover = null;
+    public Texture2D textureNormal = null;
+    public Texture2D texturePressed = null;
 
     public TextureButton(String name) {
         super(name);
@@ -30,6 +38,30 @@ public class TextureButton extends BaseButton {
 
         if (stretchMode != null) {
             builder.append("stretch_mode = ").append(stretchMode.ordinal()).append("\n");
+        }
+
+        if (textureClickMask != null) {
+            builder.append("texture_click_mask = ").append(textureClickMask).append("\n");
+        }
+
+        if (textureDisabled != null) {
+            builder.append("texture_disabled = ").append(textureDisabled).append("\n");
+        }
+
+        if (textureFocused != null) {
+            builder.append("texture_focused = ").append(textureFocused).append("\n");
+        }
+
+        if (textureHover != null) {
+            builder.append("texture_hover = ").append(textureHover).append("\n");
+        }
+
+        if (textureNormal != null) {
+            builder.append("texture_normal = ").append(textureNormal).append("\n");
+        }
+
+        if (texturePressed != null) {
+            builder.append("texture_pressed = ").append(texturePressed).append("\n");
         }
 
         return builder.toString();
---
+++ src/main/java/com/robertscerri/jgdscene/resources/BitMap.java
@@ -0,0 +1,7 @@
+package com.robertscerri.jgdscene.resources;
+
+public class BitMap extends ExtResource {
+    public BitMap(String path) {
+        this.resourcePath = path;
+    }
+}
